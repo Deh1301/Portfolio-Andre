77821831cd57cc117cce93f143b2efd1c88a7f32
Andre Calixto <calixtoandre77@gmail.com>
2024-11-24 15:26:22 -0300

    rota cronometro criada

--- Router/router.js
+++ Router/router.js
@@ -5,4 +5,8 @@ rotas.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public", "index.html"));
 });
 
+rotas.get("/cronometro",(req,res)=>{
+  res.redirect("https://lista-de-tarefas-three-xi.vercel.app/")
+})
+
 module.exports = rotas;
