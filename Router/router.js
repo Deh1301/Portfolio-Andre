112966f94ebd75c27e4cfe4d119db16881fcc624
Andre Calixto <calixtoandre77@gmail.com>
2024-11-25 20:17:30 -0300

    rotas adicionadas

--- Router/router.js
+++ Router/router.js
@@ -5,12 +5,16 @@ rotas.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public", "index.html"));
 });
 
-rotas.get("/cronometro",(req,res)=>{
+rotas.get("/lista de tarefa",(req,res)=>{
   res.redirect("https://lista-de-tarefas-three-xi.vercel.app/")
 })
 
 rotas.get("/studiosweet",(req,res)=>{
   res.redirect("https://studio-sweet.vercel.app/")
+})
+
+rotas.get("/cronometro",(req,res)=>{
+  res.redirect("https://cronometro-projeto-rust.vercel.app/")
 })
 
 module.exports = rotas;
