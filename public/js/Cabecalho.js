c78d7b6db80e3661f03b6345d3aa58157cc05681
Andre Calixto <calixtoandre77@gmail.com>
2024-12-02 22:46:10 -0300

    atualizações em rodape, click btn do contato

---
+++ public/js/Cabecalho.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded",()=>{
+    const header = document.querySelector("header")
+    const nav = document.querySelector("nav")
+    setTimeout(()=>{
+        header.style.opacity="90%"
+        header.style.transition = ".5s"
+        nav.style.opacity="90%"
+        nav.style.transition = ".5s"
+        
+
+    },1000)
+    
+    AOS.init()
+})
