bebd5e1dea11ff243b9d5d0bd60881d71ccbeaed
ANDRE CALIXTO DOS SANTOS <129115005+Deh1301@users.noreply.github.com>
2025-02-19 19:15:42 -0300

    atualizando

--- public/js/header.js
+++ public/js/header.js
@@ -1,20 +1,52 @@
-document.addEventListener("DOMContentLoaded",()=>{
+document.addEventListener("DOMContentLoaded", () => {
     const header = document.querySelector("header")
     const nav = document.querySelector("nav")
-    setTimeout(()=>{
-        header.style.opacity="90%"
+    setTimeout(() => {
+        header.style.opacity = "90%"
         header.style.transition = ".5s"
-        nav.style.opacity="90%"
+        nav.style.opacity = "90%"
         nav.style.transition = ".5s"
-        
 
-    },1000)
-    const typed = new Typed("#headerTxt",{
-        typeSpeed:50,
-        loop:true,
-        strings:['< >Bem Vindo ao meu Portfólio < / >']
+
+    }, 1000)
+    const typed = new Typed("#headerTxt", {
+        typeSpeed: 50,
+        loop: true,
+        strings: ['< >Bem Vindo ao meu Portfólio < / >']
 
     })
-    
+
     AOS.init()
+
+    const AnimationText = () => {
+        const p = document.querySelector("#p")
+
+        function AnimarP() {
+            if (p.style.marginTop === "" || p.style.marginTop === "0px") {
+                p.style.marginTop = "-40px";
+                setTimeout(() => {
+                    p.style.marginTop = "10px";
+                    p.style.transition = ".4s"
+                    p.style.opacity = "100%"
+                }, 1000);
+
+            }
+        }
+        AnimarP()
+
+        function AnimarP1() {
+
+            setTimeout(() => {
+                p.style.marginTop = "0px";
+                p.style.transition = ".4s"
+                p.style.opacity = "100%"
+            }, 1100);
+
+
+        }
+        AnimarP1()
+    }
+
+
+    AnimationText()
 })
