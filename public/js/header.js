22c4111802240afa343cf7838682973c3dc7d26d
ANDRE CALIXTO DOS SANTOS <129115005+Deh1301@users.noreply.github.com>
2025-03-12 14:54:28 -0300

    atualizando repo

--- public/js/header.js
+++ public/js/header.js
@@ -18,35 +18,83 @@ document.addEventListener("DOMContentLoaded", () => {
 
     AOS.init()
 
-    const AnimationText = () => {
-        const p = document.querySelector("#p")
-
-        function AnimarP() {
-            if (p.style.marginTop === "" || p.style.marginTop === "0px") {
-                p.style.marginTop = "-40px";
+    const loadingText = ()=>{
+        setInterval(()=>{
+            const AnimationTextP = () => {
+                const p = document.querySelector("#p")
+                if (!p) return; 
+        
+                p.style.opacity = "0"; 
+        
                 setTimeout(() => {
-                    p.style.marginTop = "10px";
-                    p.style.transition = ".4s"
-                    p.style.opacity = "100%"
-                }, 1000);
+                   p.style.opacity = "100%";
+                   p.style.marginTop = "-20px"; 
+                   p.style.transition = "0.5s";
+                
+                setTimeout(() => {
+                    p.style.marginTop = "15px"; 
+                    p.style.transition = "0.5s ";
+                    setTimeout(() => {
+                        p.style.marginTop = "0px";
+                        p.style.transition = "0.5s ";
+                       }, 500);
+                   }, 300);
+                }, 500);
+        
+          }
+          AnimationTextP()
 
-            }
+          const AnimationTextR = ()=>{
+            const r = document.querySelector("#r")
+            if(!r) return
+            r.style.opacity = "0"
+    
+            setTimeout(()=>{
+                r.style.opacity = "100%";
+                r.style.marginTop = "10px"; 
+                r.style.transition = "0.5s";
+                setTimeout(() => {
+                    r.style.marginTop = "-35px"; 
+                    r.style.transition = "0.5s ";
+                    setTimeout(() => {
+                        r.style.marginTop = "0px";
+                        r.style.transition = "0.5s ";
+                       }, 500);
+                   }, 300);
+    
+            },500)
+    
+    
         }
-        AnimarP()
+        AnimationTextR()
 
-        function AnimarP1() {
-
-            setTimeout(() => {
-                p.style.marginTop = "0px";
-                p.style.transition = ".4s"
-                p.style.opacity = "100%"
-            }, 1100);
-
-
+        const AnimationTextO = ()=>{
+            const o = document.querySelector("#o")
+            if(!o) return
+    
+            setTimeout(()=>{
+                o.style.opacity = "100%";
+                o.style.marginTop = "10px"; 
+                o.style.transition = "0.5s";
+                setTimeout(() => {
+                    o.style.marginTop = "15px"; 
+                    o.style.transition = "0.5s ";
+                    setTimeout(() => {
+                        o.style.marginTop = "0px";
+                        o.style.transition = "0.5s ";
+                       }, 500);
+                   }, 300);
+    
+            },500)
+           
         }
-        AnimarP1()
+        AnimationTextO()
+
+        },3000)
+        
     }
-
-
-    AnimationText()
+    loadingText()
+    
+    
+    
 })
